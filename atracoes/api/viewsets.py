51cade174f75b44a1acf4cf2d21048303e028be0
Anderson O. Aristides <anderson.aristides@fh.com.br>
2020-03-29 21:15:10 -0300

    Feat: Implement Search Filter API

--- atracoes/api/viewsets.py
+++ atracoes/api/viewsets.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 
 from atracoes.api.serializers import AtracaoSerializer
@@ -10,3 +11,5 @@ class AtracaoViewSet(ModelViewSet):
         """
     queryset = Atracao.objects.all()
     serializer_class = AtracaoSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('nome', 'descricao')
--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
 from core.api.serializers import PontoTuristicoSerializer
@@ -9,6 +10,8 @@ class PontoTuristicoViewSet(ModelViewSet):
     API endpoint that allows users to be viewed or edited.
     """
     serializer_class = PontoTuristicoSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ('nome', 'descricao', 'endereco__linha1')
 
     def get_queryset(self):
         id = self.request.query_params.get('id', None)
